f5d03859240c2e079a7282bd757c3fb0ac0a3eec
mageeus <82482749+mageeus@users.noreply.github.com>
2021-04-18 00:02:58 -0300

    Add files via upload

---
+++ trabaio em js.js
@@ -0,0 +1,26 @@
+
+function mostrar(){
+   
+    var nomedocarrinho = document.getElementById('Nomecarrinho')
+    var name = nomedocarrinho.value;
+
+    var outname = document.getElementById('outname')
+
+    var parcelas = document.getElementById('parcelas')
+    
+    var precocarrinho = document.getElementById('precocarrinho')
+    var preco = Number(precocarrinho.value)
+    
+    var respostinha = document.getElementById('respostinha')
+    
+    respostinha.textContent = `valor de entrada: R$${preco/2} `
+    outname.textContent = `${name}`
+    parcelas.textContent = `+12x de R$${preco/2/12}`
+    
+    
+    
+    
+    
+    }
+    
+    
